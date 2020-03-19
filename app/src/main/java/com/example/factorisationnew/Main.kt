7f1e6b66d0d5ca348cb70fde60856e9719db66e2
Julia_Anish <julia.md999@gmail.com>
2020-03-19 20:02:01 +0200

    Add tests, fixed error with unit factorization

--- app/src/main/java/com/example/factorisationnew/Main.kt
+++ app/src/main/java/com/example/factorisationnew/Main.kt
@@ -4,20 +4,20 @@ import java.util.*
 
 class Main {
     fun factor(x: Int): Array<Int> {
-        var primeNumbersArr = emptyArray<Int>()
+        var primeNumbersArr = emptyArray<Int>()     //массив с простыми числами от 2-ух до х
 
         loop@ for (i in 2..x) {
-            for (j in 2..i - 1) {
-                if (i % j == 0 && i != j) continue@loop
-            }
+            for (j in 2..i - 1) {        //то, на что мы делим, чтобы вычислить простое число
+                if (i % j == 0 && i != j) continue@loop     //если остаток =0, значить число делится еще на что-то помимо себя и 1.
+            }                                               // i != j т.к. абсолютно все числа делятся на самих себя
             primeNumbersArr += i
         }
         println(primeNumbersArr.contentToString())
 
         var i = 0
-        var xCurrent: Double = x.toDouble()
-        var factorArr = emptyArray<Int>()
-        while (true) {
+        var xCurrent: Double = x.toDouble()         //продулировали переменную и привели к double, т.к. при последнем делении некоторых чисел получалась единица с десятичным знаком, которая по сути больше единицы, но программа ее приравнивала к единице
+        var factorArr = emptyArray<Int>()           //массив с множителями х
+        while (xCurrent>1.0) {                      //для 1 нет простого числа
 
             var resDiv = xCurrent / primeNumbersArr[i]
             if (resDiv == 1.0) {
--- app/src/test/java/com/example/factorisationnew/NewTest.kt
+++ app/src/test/java/com/example/factorisationnew/NewTest.kt
@@ -8,13 +8,30 @@ class NewTest {
     fun test1(){
         val newTest = Main()
         Assertions.assertEquals("[2, 2, 3, 7]", newTest.factor(84).contentToString())
-
     }
 
     @Test
     fun test2 (){
         val newTest = Main()
         Assertions.assertEquals("[11]", newTest.factor(11).contentToString())
+    }
+
+    @Test
+    fun test3 (){
+        val newTest = Main()
+        Assertions.assertEquals("[]", newTest.factor(1).contentToString())
+    }
+
+    @Test
+    fun test4 (){
+        val newTest = Main()
+        Assertions.assertEquals("[3, 5, 37]", newTest.factor(555).contentToString())
+    }
+
+    @Test
+    fun test5 (){
+        val newTest = Main()
+        Assertions.assertEquals("[2, 7, 13, 41]", newTest.factor(7462).contentToString())
 
     }
 }
